d7bd110f45f2724e8a9e7c3c9ea007e53649f186
Linda Balounová <linda_balounova@MacBook-Air-2.local>
2023-04-11 00:01:25 +0200

    function, switching classes and ids to English

--- hra.js
+++ hra.js
@@ -1,7 +1,21 @@
 let currentPlayer = 'circle';
 
 const addClass = (event) => {
-  event.target.classList.add('board__field--circle');
+  if (currentPlayer === 'circle') {
+    currentPlayer = 'cross';
+    document.querySelector('#player').classList.remove('circle');
+    document.querySelector('#player').classList.add('cross');
+    event.target.disabled = true;
+    return event.target.classList.add('board__field--circle');
+  }
+
+  if (currentPlayer === 'cross') {
+    currentPlayer = 'circle';
+    document.querySelector('#player').classList.remove('cross');
+    document.querySelector('#player').classList.add('circle');
+    event.target.disabled = true;
+    return event.target.classList.add('board__field--cross');
+  }
 };
 
 document.querySelector('#button1').addEventListener('click', addClass);
