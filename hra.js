d193f8257491e38729577d696cef33ec81100268
Linda Balounová <linda_balounova@MacBook-Air-2.local>
2023-04-20 21:47:27 +0200

    final

--- hra.js
+++ hra.js
@@ -16,32 +16,32 @@ const addClass = (event) => {
     event.target.classList.add('board__field--cross');
     event.target.disabled = true;
   }
+
+  const buttons = document.querySelectorAll('button');
+  const allButtonsArray = Array.from(buttons).map((button) => {
+    if (button.classList.contains('board__field--circle')) {
+      return 'o';
+    }
+    if (button.classList.contains('board__field--cross')) {
+      return 'x';
+    }
+
+    return '_';
+  });
+
+  const winner = findWinner(allButtonsArray);
+  if (winner === 'o') {
+    setTimeout(() => {
+      alert('..and the winner is Mrs.Kolečko!');
+      location.reload();
+    }, 300);
+  } else if (winner === 'x') {
+    setTimeout(() => {
+      alert('..and the winner is Mr.Křížek!');
+      location.reload();
+    }, 300);
+  }
 };
-
-const buttons = document.querySelectorAll('button');
-const allButtonsArray = Array.from(buttons).map((button) => {
-  if (button.classList.contains('board__field--circle')) {
-    return 'o';
-  }
-  if (button.classList.contains('board__field--cross')) {
-    return 'x';
-  }
-
-  return '_';
-});
-
-const winner = findWinner(allButtonsArray);
-if (winner === 'o') {
-  setTimeout(() => {
-    alert('..and the winner is Mrs.Kolečko!');
-    location.reload();
-  }, 300);
-} else if (winner === 'x') {
-  setTimeout(() => {
-    alert('..and the winner is Mr.Křížek!');
-    location.reload();
-  }, 300);
-}
 
 document.querySelectorAll('button').forEach((btn) => {
   btn.addEventListener('click', addClass);
